26d3e4ff055dbaa2087eaebf3c1a4d90a200fb7c
esratncr <tuncer.esra1453@gmail.com>
2023-02-07 13:56:37 +0300

    abuot html

---
+++ JS/general.js
@@ -0,0 +1,15 @@
+/*blog date start */
+// const blogDate =document.getElementById("blogDate")
+
+// const newDate=new Date()
+
+
+// blogDate.innerText=newDate.toLocaleDateString()
+// /*replaceAll()dediğimiz zamn noktalama işaretlerini değiştiriyıruz */
+
+const blogDate =document.getElementById("blogDate")
+
+const newDate=new Date()
+
+blogDate.innerText=newDate.toLocaleDateString()
+
